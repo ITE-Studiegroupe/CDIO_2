6cea0b46a4824f3ca2735464cfc21025283f76a4
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-10-31 12:24:42 +0100

    Feltspil

--- 36_del2/src/feltspil/Terning.java
+++ 36_del2/src/feltspil/Terning.java
@@ -1,5 +1,21 @@
 package feltspil;
 
 public class Terning {
+	
+	/*Denne klasse har til formål at lave et terning-kast, der som en terning 
+	en terning i virkeligheden giver en tilfældig værdi mellem 1-6*/
+	private int terning;
+
+	private void Kast() {
+		int terning;
+		terning = ((int) (Math.random()*6)+1);
+		this.terning = terning;
+	 }
+	 //get kast
+	 public int getKast() {
+		 Kast();
+		 return terning;
+
+	}
 
 }
