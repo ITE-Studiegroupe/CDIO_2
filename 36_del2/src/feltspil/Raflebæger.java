e89e7595ded5ff7f5e35484ddae3a2921d223081
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-10-31 12:23:52 +0100

    rafle

--- 36_del2/src/feltspil/Raflebæger.java
+++ 36_del2/src/feltspil/Raflebæger.java
@@ -1,5 +1,12 @@
 package feltspil;
 
 public class Raflebæger {
+	private Terning terning = new Terning();
+//get sumTerninger
+public int getSumterninger() {
+	int t1 = terning.getKast();
+	int t2 = terning.getKast();
+	return t1+t2;
 
+  }
 }
