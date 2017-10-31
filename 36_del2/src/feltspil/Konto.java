3d202834b249e16ecc5aef2e48fb695abd71c7df
Jacob <Jacob@DESKTOP-91SF1OG.eduroam.wireless.dtu.dk>
2017-10-31 21:39:38 +0100

    tilføjet metoder til kontoklassen

--- 36_del2/src/feltspil/Konto.java
+++ 36_del2/src/feltspil/Konto.java
@@ -1,5 +1,27 @@
 package feltspil;
 
 public class Konto {
+	private int balance;
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public boolean indsætPenge(int p) {
+		int tjek = balance - p ;
+		boolean resultat;
+		if(tjek<0) {
+			resultat = false;
+					}
+		else {
+			resultat = true;
+		}
+	return resultat;
+	}
+	
 
 }
