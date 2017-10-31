a964b682d71e6d05b053891583076a4c470780cd
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-10-31 12:04:54 +0100

    Opd spiller

--- 36_del2/src/feltspil/Spiller.java
+++ 36_del2/src/feltspil/Spiller.java
@@ -1,5 +1,30 @@
 package feltspil;
 
 public class Spiller {
+	
+	/* Denne klasse har til formål at give muligheden for at spillerne får et navn
+	 * og et nr, så man kan holde styr på hvem der er spiller 1 og spiller 2.
+	 */
+	
+	//variabler
+	private String spillernavn;
+	private int spillernr;
+
+	 //set navn
+	 public void setNavn(String navn_spiller) {
+		 spillernavn = navn_spiller;
+	}
+	 //get navn
+	 public String getNavn() {
+		 return spillernavn;
+	}
+	 //set nr
+	 public void setNr(int nr_spiller) {
+		 spillernr = nr_spiller;
+	 }
+	 //get nr
+	 public int getNr() {
+		 return spillernr;
+	 }
 
 }
