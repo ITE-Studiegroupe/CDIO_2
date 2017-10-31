a6a3285dc84a4b8b4dd97e3ab73a36fdd8bb2bf9
nicol <nicol@LAPTOP-KMKH3SE0.eduroam.wireless.dtu.dk>
2017-10-31 12:21:18 +0100

    Felter

--- 36_del2/src/feltspil/Felter.java
+++ 36_del2/src/feltspil/Felter.java
@@ -1,5 +1,38 @@
 package feltspil;
 
+
 public class Felter {
+
+	public int getPoint(int nr) {
+	 	int point;	
+	 	switch(nr) {
+	 	case 2: point=250;
+	 	break;
+	 	case 3: point=-100; 
+	 	break;
+	 	case 4: point=+100; 
+	 	break;
+	 	case 5: point=-20; 
+	 	break;
+	 	case 6: point=180;
+	 	break;
+	 	case 7: point=0; 
+	 	break;
+	 	case 8: point=-70; 
+	 	break;
+	 	case 9: point=60; 
+	 	break;
+	 	case 10: point=-80; 
+	 	break;
+	 	case 11: point=-50; 
+	 	break;
+	 	case 12: point=650; 
+	 	break;
+	 	default: point=0;
+	 	}
+		 
+	 	return point;
+	
+	 	}
 
 }
