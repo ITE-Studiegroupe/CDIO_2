345330ff78808c16a4a9b93b1ff3600cd7bd4531
Nils <Nils@DESKTOP-SHRLHLB.home>
2017-11-01 17:48:25 +0100

    Opdateret Tekst klassen så den ikke indlæser besked numre ind i teksten, tilføjet kommentarer til Tekst klassen samt rettet sprogfilerne.

--- 36_del2/src/feltspil/Main.java
+++ 36_del2/src/feltspil/Main.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner s; 
 		Spiller spiller1 = new Spiller();
 		Spiller spiller2 = new Spiller();
 		Raflebæger raflebæger = new Raflebæger();
 		Felter felt = new Felter();
-		
+
 		s = new Scanner(System.in);
 		String input;
-		
-		
+
+
 		System.out.print("Indtast spiller 1's navn eller \"exit\" for at lukke: ");
-		
+
 		input = s.nextLine();
 		//Checker om "exit" er blevet indtastet, hvis sandt, lukkes programmet.
 		if(input.toLowerCase().contentEquals("exit")) {
@@ -26,11 +26,11 @@ public class Main {
 			//break;
 		}
 		else { spiller1.setNavn(input);
-				spiller1.setNummer(1);
-				
-				}
-		
-		
+		spiller1.setNummer(1);
+
+		}
+
+
 		System.out.print("Indtast spiller 2's navn: ");
 		Konto konto1 = new Konto();
 		konto1.setBalance(1000);
@@ -39,13 +39,13 @@ public class Main {
 		spiller2.setNummer(2);
 		Konto konto2 = new Konto();
 		konto2.setBalance(1000);
-		
+
 		while (true) {
 			s = new Scanner(System.in);
 			String input2;
-			
+
 			System.out.print("Det er " + spiller1.getNavn()+ " skriv kast");
-			
+
 			input2 = s.nextLine();
 			//Checker om "exit" er blevet indtastet, hvis sandt, lukkes programmet.
 			if(input2.toLowerCase().contentEquals("kast")) {
@@ -55,13 +55,13 @@ public class Main {
 				konto1.indsætPenge(penge);
 				System.out.println("Du slog " + point);
 				System.out.println("Du har dermed " + konto1.getBalance() + " på kontoen");
-	
-		}
+
+			}
 			s = new Scanner(System.in);
 			String input3;
-			
+
 			System.out.print("Det er " + spiller2.getNavn()+ " skriv kast");
-			
+
 			input3 = s.nextLine();
 			//Checker om "exit" er blevet indtastet, hvis sandt, lukkes programmet.
 			if(input3.toLowerCase().contentEquals("kast")) {
@@ -71,11 +71,11 @@ public class Main {
 				konto2.indsætPenge(penge);
 				System.out.println("Du slog " + point);
 				System.out.println("Du har dermed " + konto2.getBalance() + " på kontoen");
-		break;
+				break;
 
 
+			}
 		}
-	}
-}}
+	}}
 
 
--- 36_del2/src/feltspil/Tekst.java
+++ 36_del2/src/feltspil/Tekst.java
@@ -3,36 +3,65 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * Klassen: Tekst<br>
+ * @author gruppe 36
+ *
+ */
 public class Tekst {
 	private static StrengArray sprog = new StrengArray(new String[] {"DA", "EN"});
 	private static StrengArray tekstFelter = new StrengArray();
 	private static StrengArray tekstKnapper = new StrengArray();
 	private static StrengArray tekstBeskeder = new StrengArray();
 	private static StrengArray[] tekstArray = new StrengArray[] {tekstKnapper, tekstFelter, tekstBeskeder};
-
+	
+	/**
+	 * Indlæser sprogfil i forhold til det givne String input, 
+	 * hvis input'et ikke matcher nogle kompatible sprog, sættes dette til dansk.<br>
+	 * Tilgængelige sprog: DA (dansk) og EN (engelsk).
+	 * @param input ISO 639-1 sprog koder
+	 */
 	public static void indlæsSprog(String input) {
-		// TODO Auto-generated method stub
-		File fil = new File("src/sprog/EN");
+		File fil;
+		String sti;
 		
-		
-		if(sprog.indeholder(input)) {
+		if(sprog.indeholder(input.toUpperCase())) {
 			switch(sprog.indeksAf(input)) {
 			case 0:
-				fil = new File("src/sprog/DA");
+				sti = "src/sprog/DA";
 				break;
 			case 1:
-				fil = new File("src/sprog/EN");
+				sti = "src/sprog/EN";
 				break;
+			default:
+				sti = "src/sprog/DA";
 			}
 		}
+		else sti = "src/sprog/DA";
+		
+		fil = new File(sti);
 		læsTekst(fil);
 	}
 	
-	public static String getTekst(int t, int i) {
-		if (t>=tekstArray.length||t<0) return "index out of bounds";
-		return tekstArray[t].getFraIndeks(i);
+	/**
+	 * Returnerer den valgte tekst fra tekstArray i forhold til givne kategori nummer k og tekst indeks i.
+	 * @param k tekst kategori
+	 * @param i tekst indeks
+	 * @return String med den valgte tekst eller "index out of bounds" hvis denne ikke findes.
+	 */
+	public static String getTekst(int k, int i) {
+		if (k>=tekstArray.length||k<0) return "index out of bounds";
+		return tekstArray[k].getFraIndeks(i);
 	}
 	
+	/**
+	 * Indlæser sprogfilen ind i StrengArray array'en tekstArray i deres respektive pladser i forhold til tekst formateringen<br>
+	 * I følgende rækkefølge:<br>
+	 * - StrengArray tekstKnapper<br>
+	 * - StrengArray tekstFelter<br>
+	 * - StrengArray tekstBeskeder
+	 * @param f Filen som skal læses
+	 */
 	private static void læsTekst(File f) {
 		Scanner sc;
 		int i = -1;
@@ -41,13 +70,13 @@ public class Tekst {
 			while(sc.hasNextLine()) {
 				String st = sc.nextLine();
 				
-				if(st.contains("//")||st.equals("")) continue;
-				else if (st.contains("@@")) {
+				if(st.contains("//")||st.equals("")) continue; // linjer der starter med "//" eller tomme linjer springes over.
+				else if (st.contains("@@")) { // linjer der starter med "@@" giver tegn på at der skal skiftes indeks i tekstArray[], dvs ny kategori.
 					i++;
 					continue;
 				}
 				
-				tekstArray[i].tilføj(st);
+				tekstArray[i].tilføj(st.substring(3));
 			}
 			sc.close();
 		} catch (FileNotFoundException e) {
