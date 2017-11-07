ee834bdf55a7539c25192676a1bcf64769be5094
Jacob <Jacob@DESKTOP-91SF1OG>
2017-11-07 10:16:05 +0100

    lavet en JUnit test, og oprettet en test pakke

---
+++ 36_del2/src/test/MainTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import feltspil.Konto;
+
+public class MainTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1000() {
+		Konto konto = new Konto();
+		konto.setBalance(1000);
+		int tal = 0;
+		int antalAfvist = 0;
+		while(tal < 1000) {
+			boolean indsæt = konto.indsætPenge(-1001);
+			if(!indsæt) {
+				antalAfvist++;
+				tal++;
+			}
+			else {tal++;}
+		}
+		int actual = antalAfvist;
+		int expected = 1000;
+	System.out.println(konto.getBalance());
+	assertEquals(expected,actual);
+		}
+
+	}
+
