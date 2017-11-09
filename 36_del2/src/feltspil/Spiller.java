114308d343400102135c95978a2e786082512447
Mads Schultz <s175123@student.dtu.dk>
2017-11-09 15:33:46 +0100

    Sletter metoden setSpillerNummer fra spiller.java. Metoden bliver ikke brugt

--- 36_del2/src/feltspil/Spiller.java
+++ 36_del2/src/feltspil/Spiller.java
@@ -3,33 +3,33 @@ package feltspil;
 /**
  * 
  * Kallsen Spiller
+ * 
  * @author gruppe 36
  * 
  */
 public class Spiller {
-	
-	/** Denne klasse har til formål at give muligheden for at spillerne får et navn
+
+	/**
+	 * Denne klasse har til formål at give muligheden for at spillerne får et navn
 	 * og et nr, så man kan holde styr på hvem der er spiller 1 og spiller 2.
 	 */
-	
-	//variabler
+
+	// variabler
 	private String spillernavn;
 	private int spillernr;
 
-	 //Sætter spillerens navn
-	 public void setNavn(String navn_spiller) {
-		 spillernavn = navn_spiller;
+	// Sætter spillerens navn
+	public void setNavn(String navn_spiller) {
+		spillernavn = navn_spiller;
 	}
-	 //Returnerer spillerens navn
-	 public String getNavn() {
-		 return spillernavn;
+
+	// Returnerer spillerens navn
+	public String getNavn() {
+		return spillernavn;
 	}
-	 //Sætter spillerens nummer
-	 public void setNummer(int nr_spiller) {
-		 spillernr = nr_spiller;
-	 }
-	 //Returner spillerens nummer
-	 public int getNummer() {
-		 return spillernr;
-	 }
+
+	// Returner spillerens nummer
+	public int getNummer() {
+		return spillernr;
+	}
 }
